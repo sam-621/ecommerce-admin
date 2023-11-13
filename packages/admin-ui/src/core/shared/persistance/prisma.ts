864c9213e445176bb862e545d0d29288d2cccd29
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-11-12 17:38:40 -0700

    feat(admin-ui):  add prisma client

---
+++ packages/admin-ui/src/core/shared/persistance/prisma.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+import { PrismaClient } from '@prisma/client';
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export { prisma };
