d8f758efd733644978a24a1e529d233442426a2d
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-11-15 18:31:26 -0700

    feat(components):  update dorpxone to show preview

--- packages/admin-ui/src/app/admin/inventory/components/table-actions.tsx
+++ packages/admin-ui/src/app/admin/inventory/components/table-actions.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import type { Row } from '@tanstack/react-table';
 import { MoreHorizontalIcon } from 'lucide-react';
 import type { FC } from 'react';
--- packages/admin-ui/src/components/theme/dropzone.tsx
+++ packages/admin-ui/src/components/theme/dropzone.tsx
@@ -1,9 +1,32 @@
-import { UploadCloudIcon } from 'lucide-react';
-import { type DetailedHTMLProps, forwardRef, type InputHTMLAttributes } from 'react';
+'use client';
+
+import { UploadCloudIcon, XIcon } from 'lucide-react';
+import Image from 'next/image';
+import {
+  type DetailedHTMLProps,
+  forwardRef,
+  type InputHTMLAttributes,
+  useRef,
+  useState
+} from 'react';
+
+import { Button } from './button';
 
 export const Dropzone = forwardRef<HTMLInputElement, Props>(function Dropzone({ ...rest }, ref) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [file, setFile] = useState<File | null>(null);
+  const previewUrl = file ? URL.createObjectURL(file) : null;
+
+  const handleRemove = () => {
+    setFile(null);
+
+    if (!inputRef.current) return;
+
+    inputRef.current.value = '';
+  };
+
   return (
-    <div className="flex items-center justify-center w-full">
+    <div className="flex flex-col gap-4 w-full">
       <label
         htmlFor="dropzone-file"
         className="flex flex-col items-center justify-center w-full h-64 border-2 border-input hover:border-ring border-dashed rounded-lg cursor-pointer  transition-colors"
@@ -12,8 +35,46 @@ export const Dropzone = forwardRef<HTMLInputElement, Props>(function Dropzone({ 
           <UploadCloudIcon width={24} />
           <p className="text-sm text-gray-500 dark:text-gray-400">Accepts .jpg and .png</p>
         </div>
-        <input ref={ref} {...rest} id="dropzone-file" type="file" className="hidden" />
+        <input
+          ref={ref ?? inputRef}
+          {...rest}
+          onChange={e => e.target.files?.length && setFile(e.target.files[0])}
+          id="dropzone-file"
+          type="file"
+          className="hidden"
+        />
       </label>
+      {previewUrl && (
+        <div className="flex flex-col gap-3">
+          <span>Upload:</span>
+          <div className="flex justify-between items-center">
+            <div className="flex gap-2 items-center">
+              <Image
+                src={previewUrl}
+                alt="Imagen subida"
+                width={60}
+                height={60}
+                className="object-cover rounded-md"
+              />
+              <div>
+                <p className="text-foreground text-sm font-medium">{file?.name}</p>
+                <span className="text-muted-foreground text-sm font-normal ">
+                  {(file?.size ?? 0) * 0.001} KB
+                </span>
+              </div>
+            </div>
+            <div>
+              <Button
+                onClick={handleRemove}
+                variant={'ghost'}
+                className="p-0 h-fit hover:bg-transparent"
+              >
+                <XIcon className="text-muted-foreground" />
+              </Button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 });
