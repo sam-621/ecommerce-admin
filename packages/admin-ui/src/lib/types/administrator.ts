343a9ad645b637b2b0400ca7d0375f93f0ec7f77
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-11-15 22:10:32 -0700

    feat(lib):  add types

---
+++ packages/admin-ui/src/lib/types/administrator.ts
@@ -0,0 +1,7 @@
+export interface Administrator {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+  username: string;
+  password: string;
+}
---
+++ packages/admin-ui/src/lib/types/product.ts
@@ -0,0 +1,15 @@
+export interface Product {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+  name: string;
+  slug: string;
+  sku: string;
+  description: string;
+  image: string;
+  price: number;
+  comparisonPrice: number;
+  weight: number;
+  stock: number;
+  enabled: boolean;
+}
