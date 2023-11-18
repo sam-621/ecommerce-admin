a607d3725dfc8c8b2b6ff350d8bc3fd55407be41
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-11-17 22:04:44 -0700

    feat(lib):  add notifications

---
+++ packages/admin-ui/src/lib/notification/index.ts
@@ -0,0 +1,58 @@
+import { toast } from 'sonner';
+
+/**
+ * Notification module
+ *
+ * @example
+ * import { notification } from '@lib/notification';
+ *
+ * // Display a success notification
+ * notification.success('Hello World');
+ *
+ * // Display an error notification
+ * notification.error('Hello World');
+ *
+ * // Dismiss a notification after 5 seconds
+ * const id = notification.success('Hello World');
+ *
+ * setTimeout(() => {
+ *  notification.dismiss(id);
+ * }, 5000);
+ *
+ */
+export const notification = {
+  /**
+   * Display a success notification with the provided message
+   * @param msg message to display
+   * @returns notification id
+   */
+  success: (msg: string) => {
+    return toast.success(msg);
+  },
+
+  /**
+   * Display an error notification with the provided message
+   * @param msg message to display
+   * @returns notification id
+   */
+  error: (msg: string) => {
+    return toast.error(msg);
+  },
+
+  /**
+   * Dismiss a notification by the notification id provided
+   * @param id notification id returned from other notification methods
+   *
+   * @example
+   * const id = notification.success('Hello World');
+   *
+   * // Dismiss the notification after 5 seconds
+   * setTimeout(() => {
+   *  notification.dismiss(id);
+   * }, 5000);
+   *
+   */
+  dismiss: (id: string) => {
+    toast.dismiss(id);
+  }
+};
