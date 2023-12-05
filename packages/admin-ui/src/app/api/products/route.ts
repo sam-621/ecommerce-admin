197663b55bb1934f70473570e822717117fef421
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-12-04 21:53:16 -0700

    feat(api):  add enable validation in products

--- packages/admin-ui/src/app/api/products/route.ts
+++ packages/admin-ui/src/app/api/products/route.ts
@@ -3,6 +3,7 @@ import { RouteResponse } from '@/lib/utils';
 
 export const GET = async (req: Request) => {
   const products = await ProductRepository.getMany();
+  const productToSend = products.filter(product => product.enabled);
 
-  return Response.json(new RouteResponse(products, ['OK']));
+  return Response.json(new RouteResponse(productToSend, ['OK']));
 };
